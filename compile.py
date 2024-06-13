5ab4bea0d7f4e6612865939662d82f820bed54d8
xyfuture <xyfuture@foxmail.com>
2024-06-13 16:36:36 +0800

    update arg parser

--- compile.py
+++ compile.py
@@ -1,4 +1,5 @@
 import argparse
+import pickle
 
 from Networks.auto_encoder import AutoEncoder, get_autoencoder
 from Networks.yolo import get_yolovgg
@@ -16,33 +17,17 @@ from utils import create_input_tensor
 parser = argparse.ArgumentParser(description='Compiler for CIM')
 
 # 通过config 指定 mapping 方式
-parser.add_argument('--mapping', '-m', type=str, default='performance',help='Mapping Strategy : performance or utilization')
+parser.add_argument('--strategy', '-s', type=str, default='performance',
+                    help='Mapping Strategy : performance or utilization')
+parser.add_argument('--target', '-t', type=str, default='trace', help="Output Target: trace or mapping")
+parser.add_argument('--mapping', '-m', type=str, default=None, help='Mapping File Path')
+parser.add_argument('--output', '-o', type=str, default=None, help='Output File Path: Trace or Mapping')
 parser.add_argument('--config', '-c', type=str, default='example/resnet_config.json', help='Configuration File Path')
-parser.add_argument('--trace', '-t', type=str, default='example/resnet_trace.json', help='Trace File Path')
+# parser.add_argument('--trace', '-t', type=str, default='example/resnet_trace.json', help='Trace File Path')
 parser.add_argument('--network', '-n', type=str, default='resnet', help='Network Name')
 parser.add_argument('--verbose', '-v', action='store_true', help='Report All Module Results')
 
 args = parser.parse_args()
-
-
-# def resnet_run():
-#     net = resnet18()
-#     net.mapping()
-#
-#     input_tensor = create_input_tensor((32, 32), 3)
-#     output_tensor = net(input_tensor)
-#     return net
-#
-#
-# def autoencoder_run():
-#     # TODO 测试
-#     net = AutoEncoder()
-#     net.mapping()
-#
-#     input_tensor = create_input_tensor((1,),1024)
-#     output_tensor = net(input_tensor)
-#     return net
-
 
 if __name__ == '__main__':
 
@@ -50,7 +35,6 @@ if __name__ == '__main__':
     print(f'{split_line}\n'
           f'{"Compiler Start".center(70)}\n'
           f'{split_line}\n')
-
 
     graph = MicroGraph()
     MicroGraph.current_graph = graph
@@ -66,11 +50,11 @@ if __name__ == '__main__':
     Chip.current_chip = chip
 
     if args.network == 'resnet':
-        net,input_tensor = get_resnet18()
+        net, input_tensor = get_resnet18()
     elif args.network == 'autoencoder':
-        net,input_tensor = get_autoencoder()
+        net, input_tensor = get_autoencoder()
     elif args.network == 'yolo':
-        net,input_tensor = get_yolovgg()
+        net, input_tensor = get_yolovgg()
     else:
         raise "Set your own network here"
 
@@ -78,42 +62,51 @@ if __name__ == '__main__':
           f'{"Network Mapping".center(70)}\n'
           f'{split_line}\n')
 
-    # 进行mapping， 根据 config 中的mapping方式进行映射
-    net.mapping()
+    if args.target == 'mapping':
+        net.mapping()
 
+        # 输出mapping的结果
+        if args.verbose:
+            split_line = "=" * 70
+            print(f'{split_line}\n'
+                  f'{"Mapping Results".center(70)}\n'
+                  f'{split_line}\n')
+            # 输出Mapping
+            print(report_mapping_status(net))
 
+        with open(args.output, 'wb') as f:
+            pickle.dump(net, f)
+    elif args.target == 'trace':
+        if args.mapping is None:
+            # 进行mapping， 根据 config 中的strategy方式进行映射
+            net.mapping()
+        else:
+            with open(args.mapping, 'rb') as f:
+                net = pickle.load(f)
+        # 输出mapping的结果
+        if args.verbose:
+            split_line = "=" * 70
+            print(f'{split_line}\n'
+                  f'{"Mapping Results".center(70)}\n'
+                  f'{split_line}\n')
+            # 输出Mapping
+            print(report_mapping_status(net))
 
-    # 输出mapping的结果
-    if args.verbose:
-        split_line = "=" * 70
         print(f'{split_line}\n'
-              f'{"Mapping Results".center(70)}\n'
+              f'{"Code Generation".center(70)}\n'
               f'{split_line}\n')
-        # 输出Mapping
-        print(report_mapping_status(net))
+        # 进行编译流程
+        output_tensor = net(input_tensor)
 
+        # run some optimization pass
+        pad_to_core(graph)
+        find_right_input_op(graph)
 
+        # lowering
+        graph.lower_to_machine_op()
+        chip.lower_to_inst()
 
-    print(f'{split_line}\n'
-          f'{"Code Generation".center(70)}\n'
-          f'{split_line}\n')
-    # 进行编译流程
-    output_tensor = net(input_tensor)
+        # write to file
+        chip.dump_output_to_file(args.output)
 
-    # run some optimization pass
-    pad_to_core(graph)
-    find_right_input_op(graph)
-
-    # debug
-    # topo_node_list = topo_sort(graph)
-    # for node in topo_node_list:
-    #     print(node.micro_op.full_info())
-
-    # lowering
-    graph.lower_to_machine_op()
-    chip.lower_to_inst()
-
-    # write to file
-    chip.dump_output_to_file(args.trace)
-
-    print(f"File Write To {args.trace}")
+        print(f"File Write To {args.output}")
